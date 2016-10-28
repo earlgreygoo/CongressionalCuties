9712fcf52922edcf59866967ebf74638464a4678
E-David <ericdavid365@gmail.com>
2016-10-28 15:01:42 -0500

    Add function to handle null info, cleaned htmlString

--- congressionalCuties/scripts/script.js
+++ congressionalCuties/scripts/script.js
@@ -12,25 +12,44 @@ var handleRetrievedData = function(apiResponse) {
     console.log(apiResponse)
     for ( var i = 0; i < legislatorsArray.length; i ++) {
     	legislator = legislatorsArray[i]
-        var legislatorname = legislator.first_name + " " + legislator.last_name
-        
-        var legislatorstats = legislator.title + "--" + legislator.party + "-" + legislator.state_name
-        
+      var legislatorname = legislator.first_name + " " + legislator.last_name
+      var legislatorstats = legislator.title + "--" + legislator.party + "-" + legislator.state_name
+      
+      //used for more declarative programming below
+      legislator.get = handleNullInfo
 
-
-       
-
-
-
-       htmlString += '<div class="legislator">'
-       htmlString += '<h1 class="name">' + legislatorname + '</h1>'
-       htmlString += '<p class="stats">' + legislatorstats + '</p>'
-       htmlString += '<ul class = "contactinfo">' + "<li> email: " + legislator.oc_email + "</li> <li> website: " + legislator.website + "</li> <li> facebook: " + legislator.facebook_id + "</li> <li> twitter: " + legislator.twitter_id + "</li> </ul>"
-       htmlString += '</div>'
+      htmlString += '<div class="legislator">'
+      htmlString += '<h1 class="name">' + legislatorname + '</h1>'
+      htmlString += '<p class="stats">' + legislatorstats + '</p>'
+      htmlString += '<ul class = "contactinfo">'
+      htmlString +=   "<li> email: " + legislator.get("oc_email") + "</li>"
+      htmlString +=   "<li> website: " + legislator.get("website") + "</li> "
+      htmlString +=   "<li> facebook: " + legislator.get("facebook_id") + "</li>" 
+      htmlString +=   "<li> twitter: " + legislator.get("twitter_id") + "</li>"
+      htmlString += "</ul>"
+      htmlString += '</div>'
     }
     
     legislatorContainer.innerHTML = htmlString
-    
+}
+
+//use ternary operator to check if property of an object exists. True = return property. False = return "no <propName> available"
+var handleNullInfo = function(prop) {
+  return this[prop] ? this[prop] : 'no ' + cleanInfo(prop) + ' available'
+}
+
+//Cleans specific data for the handleNullInfo string output if none are available.
+var cleanInfo = function(word) {
+  if(word ==="oc_email"){
+    return "email"
+  }
+  if(word ==="facebook_id"){
+    return "facebook"
+  }
+  if(word ==="twitter_id"){
+    return "twitter"
+  }
+  return word
 }
 
 
